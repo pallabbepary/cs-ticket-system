59eafa1f6688c779028720e43b965439badf3df1
pallabbepary <palllabbepary@gmail.com>
2026-03-23 02:01:23 +0600

    Updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import bgImg from '../../assets/Clippath.png'
 
 const Banner = ({selectedCards, resolvedTasks}) => {
     return (
-        <main className='w-11/12 mx-auto my-15 '>
+        <main className='w-11/12 mx-auto md:my-15'>
             <section className=' grid grid-cols-1 md:grid-cols-2 gap-8'>
                 
                     <section className='relative bg-[linear-gradient(135deg,#5f2cff,#7c4dff,#b56cff)] text-white h-65 flex flex-col items-center justify-center rounded-xl' >
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,50 @@
-import React from 'react';
+
+import { Menu, X } from 'lucide-react';
+import React, { useState } from 'react';
+
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
     return (
-        <header className='shadow '>
+        <header className='md:shadow '>
             <nav className='w-11/12 mx-auto py-4'>
                 <section>
-                    <div class=" ">
-                        <div class="flex-1">
-                            <a class="text-xl">
-                            
-                            </a>
-                        </div>
+                    <div class="">
+                        
                         <div class="flex justify-between">
+                           
+                            <span className='flex items-center' onClick={() => setOpen(!open)}>
+                                
+                                 {
+                                open ? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>
+                            }
+                            
                             <div>
-                                <a class="btn bg-no-repeat font-bold btn-ghost text-2xl">CS — Ticket System</a>
+                                <a class="btn bg-no-repeat font-bold btn-ghost pl-18 md:pl-0 md:text-2xl text-lg">CS — Ticket System</a>
                             </div>
-                            <div class="md:flex gap-8 items-center hidden md:block ">
-                                <button class = "font-semibold"><a href="">Home</a></button>
-                                <button class = "font-semibold"><a href="">FAQ</a></button>
-                                <button class = "font-semibold"><a href="">Changelog</a></button>
-                                <button class = "font-semibold"><a href="">Blog</a></button>
-                                <button class = "font-semibold"><a href="">Download</a></button>
-                                <button class = "font-semibold"><a href="">Contact</a></button>
+                            </span>
+                            <div class="md:flex gap-8 items-center hidden md:block">
+                                <a class = "font-semibold" href="/home">Home</a>
+                                <a class = "font-semibold" href="/faq">FAQ</a>
+                                <a class = "font-semibold" href="/changelog">Changelog</a>
+                                <a class = "font-semibold" href="/blog">Blog</a>
+                                <a class = "font-semibold" href="/download">Download</a>
+                                <a class = "font-semibold" href="/contact">Contact</a>
                                 <button class = "btn rounded-lg text-white  font-bold text-[16px] bg-gradient-to-r from-indigo-500 to-[#422ad5]"><a href="">+ New Ticket</a></button>
                             </div>
                         
                         </div>
+                        <div class={`ml-8  grid md:hidden duration-1000 ${open ? "top-15 " : "-top-60 absolute"}`}>
+                                <a class = "font-semibold" href="/home">Home</a>
+                                <a class = "font-semibold" href="/faq">FAQ</a>
+                                <a class = "font-semibold" href="/changelog">Changelog</a>
+                                <a class = "font-semibold" href="/blog">Blog</a>
+                                <a class = "font-semibold" href="/download">Download</a>
+                                <a class = "font-semibold" href="/contact">Contact</a>
+                               <a class = "btn rounded-lg text-white  md:font-bold md:text-[16px] bg-gradient-to-r from-indigo-500 to-[#422ad5]" href="/newticket">+ New Ticket</a>
+                            </div>
                     </div>
                 </section>
             </nav>
